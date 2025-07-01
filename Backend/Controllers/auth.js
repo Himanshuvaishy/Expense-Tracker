ece0a450975f791b69bbad4dd11d44d556d64ead
Himanshu <vaishhimanshu83170@gmail.com>
2025-07-01 17:58:22 +0530

    authentication done

---
+++ Backend/Controllers/auth.js
@@ -0,0 +1,40 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import router from "../routes/authRoutes.js";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userExisting = await User.findOne({ email });
+    if (userExisting)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ email, password: hashedPassword });
+
+    res.status(200).json({ message: "User created successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+ const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isCorrect = await bcrypt.compare(password, user.password);
+    if (!isCorrect) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1d' });
+    res.status(200).json({ token, user: user.email });
+
+
+  } catch (err) {}
+};
+
+export { registerUser, loginUser };
--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,30 @@
-import express from'express'
-import dotenv from'dotenv';
-import connectDB from'./config/database.js';
- 
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
 
-
-app.get('/', (req, res) => {
-  res.send('API running');
+app.get("/", (req, res) => {
+  res.send("API running");
 });
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-
     //* DB ready then server starts
     await connectDB();
     app.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
     });
   } catch (err) {
-    console.error(' Failed to connect to database:', err.message);
+    console.error(" Failed to connect to database:", err.message);
     process.exit(1);
   }
 };

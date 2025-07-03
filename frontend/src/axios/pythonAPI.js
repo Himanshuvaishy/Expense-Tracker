eb787f7f01fd162c262e606dff16237cffeaae50
Himanshu <vaishhimanshu83170@gmail.com>
2025-07-03 12:09:45 +0530

    changes in live url api

--- frontend/src/axios/pythonAPI.js
+++ frontend/src/axios/pythonAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const pythonAPI = axios.create({
-  baseURL: "https://expense-tracker-1-03il.onrender.com/api", // ✅ Your Flask backend
+  baseURL: "https://expense-tracker-1-03il.onrender.com", // ✅ Your Flask backend
   withCredentials: true,
 });
 
